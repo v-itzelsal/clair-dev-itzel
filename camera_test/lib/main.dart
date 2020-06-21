83c7ec1d939a5dd8390f9459c4dba8f3223c4c20
Sabastian <sabastianp@hotmail.ca>
2020-06-21 11:24:33 -0700

    fixed: ensures app is initialized before using cameras

--- camera_test/lib/main.dart
+++ camera_test/lib/main.dart
@@ -4,7 +4,17 @@ import 'package:camera/camera.dart';
 
 List<CameraDescription> cameras;
 
+/*
+
+Exception has occurred.
+FlutterError (ServicesBinding.defaultBinaryMessenger was accessed before the binding was initialized.
+If you're running an application and need to access the binary messenger before `runApp()` has been called (for example, during plugin initialization), then you need to explicitly call the `WidgetsFlutterBinding.ensureInitialized()` first.
+If you're running a test, you can call the `TestWidgetsFlutterBinding.ensureInitialized()` as the first line in your test's `main()` method to initialize the binding.)
+
+*/
+
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras();
   runApp(CameraApp());
 }
